68c130f2786416a20f9d09faf5714f0f6186be28
Jodd-Bamsyt <arorajivansh@gmail.com>
2025-11-30 01:38:52 +0530

    Fix Prisma build issue + deploy setup

--- actions/resume.js
+++ actions/resume.js
@@ -1,5 +1,7 @@
 "use server";
 
+export const runtime = "nodejs";
+
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
